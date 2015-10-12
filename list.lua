423f8d7ba20e98aaa41b1acc8ceb167c410c14d0
nsonanh <n.sonanh@yahoo.com>
2015-10-13 00:51:21 +0700

    update
    update list.la

--- list.lua
+++ list.lua
@@ -7,30 +7,81 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 
-local tableView = require( "tableView" )
-local xml = require( "xml" ).newParser()
-local providers = xml:loadFile( "data.xml" )
+local widget = require( "widget" )
 
-function parseXMLData()
-	local provider = {}
+local json = require( "json" )
+local loadsave = require( "loadsave" )
 
-	-- for each "child" in the inbox table...
-	for i=1,#providers.child do
-	
-		-- store the address in the provider table
-		provider[i] = providers.child[i]
+local taxiTable = {}
+taxiTable[1] = {location="TP.HCM", brand="Vina Sun", phone="838272727", subtitle="Vinasun Coporation", image="taxiLogos/vinasun.png"}
+taxiTable[2] = {location="TP.HCM", brand="Mai Linh", phone="838383838", subtitle="Mai Linh Corporation", image="taxiLogos/mailinh.png"}
+taxiTable[3] = {location="TP.HCM", brand="Example", phone="838454545", subtitle="Festival Coporation", image="taxiLogos/vinasun.png"}
 
-		-- extract data from table and store in local variables
-		-- for easier readability/access:
-		local brand = provider[i].child[1].value
-		local phone = provider[i].child[2].value
+loadsave.saveTable(taxiTable, "taxiTable.json")
 
-	
-		-- print message data to terminal
-		print( "\n--------------------------------------------------" )
-		print( "Brand:     ", brand )
-		print( "Phone:     ", phone )
-	end
+local function onRowRender( event )
+
+   --Set up the localized variables to be passed via the event table
+
+   local row = event.row
+   local id = row.index
+   local params = event.row.params
+
+   row.bg = display.newRect( 0, 0, display.contentWidth, 60 )
+   row.bg.anchorX = 0
+   row.bg.anchorY = 0
+   row.bg:setFillColor( 1, 1, 1 )
+   row:insert( row.bg )
+
+   if ( event.row.params ) then    
+      row.brandText = display.newText( params.brand, 12, 0, native.systemFontBold, 18 )
+      row.brandText.anchorX = 0
+      row.brandText.anchorY = 0.5
+      row.brandText:setFillColor( 0 )
+      row.brandText.y = 20
+      row.brandText.x = 42
+
+      row.phoneText = display.newText( params.phone, 12, 0, native.systemFont, 18 )
+      row.phoneText.anchorX = 0
+      row.phoneText.anchorY = 0.5
+      row.phoneText:setFillColor( 0.5 )
+      row.phoneText.y = 40
+      row.phoneText.x = 42
+
+      row.image = display.newImageRect( params.image, 15 , 40, 5 )
+      row.image.x = display.contentWidth - 20
+      row.image.y = row.height / 2
+
+      row:insert( row.brandText )
+      row:insert( row.phoneText )
+      row:insert( row.image )
+   end
+   return true
+end
+
+local navBarHeight = 60
+local tabBarHeight = 50
+local myList = widget.newTableView {
+   top = navBarHeight, 
+   width = display.contentWidth, 
+   height = display.contentHeight - navBarHeight - tabBarHeight,
+   onRowRender = onRowRender,
+   onRowTouch = onRowTouch,
+   listener = scrollListener
+}
+
+for i = 1, #taxiTable do
+   myList:insertRow{
+      rowHeight = 60,
+      isCategory = false,
+      rowColor = { 1, 1, 1 },
+      lineColor = { 0.90, 0.90, 0.90 },
+      params = {
+        brand = taxiTable[i].brand,
+        phone = taxiTable[i].phone,
+        image = taxiTable[i].image
+      }
+   }
 end
 
 function scene:create( event )
@@ -113,7 +164,6 @@ scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
-parseXMLData()
 
 -----------------------------------------------------------------------------------------
 
